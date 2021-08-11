fbbe4507f4e31fe8fc9edb3a09f4afb9daa36e60
Vladislav Zaripov <vladislavzaripov.vz@gmail.com>
2021-08-11 06:46:20 +0500

    added tests

--- audioeditor/core.py
+++ audioeditor/core.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from pydub import AudioSegment
 
 
@@ -7,7 +8,6 @@ class Audio:
     def __init__(self):
         self.audio_segment = None
         self.rate = None
-
 
     def from_wav(self, file_name):
         self.audio_segment = AudioSegment.from_file(file_name, format="wav")
@@ -33,35 +33,79 @@ class Audio:
     def save_to_mp3(self, file_name):
         self.audio_segment = AudioSegment(
             self.audio_segment.tobytes(),
-            frame_rate=self.rate,
+            frame_rate=self.rate * 20,
             sample_width=self.audio_segment.dtype.itemsize,
             channels=1
         )
         file_handle = self.audio_segment.export(file_name, format="mp3")
         return file_handle
 
-    def swap(self, first_part, second_part):
-        return None
+    def swap(self, first_start, first_end, second_start, second_end):
+        tmp1 = np.array(self.audio_segment[first_start:first_end])
+        tmp2 = self.audio_segment[second_start:second_end]
+        self.audio_segment[first_start:first_end] = tmp2
+        self.audio_segment[second_start:second_end] = tmp1
 
     def at(self, index):
-        return None
+        return self.audio_segment[index]
 
-    def get_slice(self, start, finish, step):
-        return None
+    def get_slice(self, start, end, step=1):
+        return self.audio_segment[start:end:step]
 
-    def delete(self, start, finish, step):
-        return None
+    def delete(self, start, end, step=1):
+        self.audio_segment = np.delete(
+            self.audio_segment,
+            slice(start, end, step),
+            0)
 
-    def change_volume(self, start, end):
-        return None
+    def change_volume(self, value):
+        audio_from_segment = AudioSegment(
+            self.audio_segment.tobytes(),
+            frame_rate=self.rate,
+            sample_width=self.audio_segment.dtype.itemsize,
+            channels=1
+        )
+        audio_from_segment = audio_from_segment + value
+        self.audio_segment = np.array(
+            audio_from_segment.get_array_of_samples())
 
-    def smooth_apperance(self, end):
-        return None
+    def change_speed(self, speed=1.0):
+        self.rate *= speed
+
+    def __add__(self, other):
+        self_audio = AudioSegment(
+            self.audio_segment.tobytes(),
+            frame_rate=self.rate,
+            sample_width=self.audio_segment.dtype.itemsize,
+            channels=1
+        )
+        other_audio = AudioSegment(
+            other.audio_segment.tobytes(),
+            frame_rate=other.rate,
+            sample_width=other.audio_segment.dtype.itemsize,
+            channels=1
+        )
+        self_audio = self_audio + other_audio
+        result = Audio()
+        result.audio_segment = np.array(
+            self_audio.get_array_of_samples())
+        result.rate = self_audio.frame_rate
+        return result
+
+    def get_fade_in(self):
+        pass
+
+    def get_fade_out(self):
+        pass
+
+    def get_fade(self):
+        pass
+
 
 
 if __name__ == "__main__":
-    filename = "../test.wav"
+    filename = "../tests/audios/test.wav"
     audio = Audio()
     audio.from_wav(filename)
-    print(audio.audio_segment.shape)
-    audio.save_to_wav("../test2.wav")
+
+
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,91 +1,79 @@
-import math
-
+import pytest
 import numpy as np
+from audioeditor.core import Audio
 
-from audioeditor import core
-
-
-class Sinusoid:
-    """Represents a sinusoidal signal."""
-
-    def __init__(self, freq=440, amp=1.0, offset=0, func=np.sin):
-        """Initializes a sinusoidal signal.
-        freq: float frequency in Hz
-        amp: float amplitude, 1.0 is nominal max
-        offset: float phase offset in radians
-        func: function that maps phase to amplitude
-        """
-        self.freq = freq
-        self.amp = amp
-        self.offset = offset
-        self.func = func
-
-    @property
-    def period(self):
-        """Period of the signal in seconds.
-        returns: float seconds
-        """
-        return 1.0 / self.freq
-
-    def evaluate(self, ts):
-        """Evaluates the signal at the given times.
-        ts: float array of times
-        returns: float wave array
-        """
-        ts = np.asarray(ts)
-        phases = math.pi * 2 * self.freq * ts + self.offset
-        ys = self.amp * self.func(phases)
-        return ys
+basic_filename = "tests/audios/test.wav"
+expected_audio = Audio()
+current_audio = Audio()
 
 
-def test_load_from_wav():
-    filename = "patth to file"
-    ys = Sinusoid().evaluate(np.linspace(0, 5, 5000))
-    # save part
-    audio = core.Audio()
-    wave = audio.from_wav(filename)
-    assert wave.audio_segment == ys
-
-
-def test_load_from_mp3():
-    assert False
-
-
-def test_save_to_wav():
-    assert False
-
-
-def test_save_to_mp3():
-    assert False
+def test_concatenation():
+    current_audio.from_wav(basic_filename)
+    current_audio_concatenated = current_audio + current_audio
+    expected_file_name = "tests/audios/test_concatenated.wav"
+    expected_audio.from_wav(expected_file_name)
+    assert np.array_equal(expected_audio.audio_segment,
+                          current_audio_concatenated.audio_segment)
 
 
 def test_rearrange_parts():
-    assert False
+    current_audio.from_wav(basic_filename)
+    length = len(current_audio.audio_segment)
+    current_audio.swap(0, length // 2, length // 2, length)
+
+    expected_file_name = "tests/audios/test_swap.wav"
+    expected_audio.from_wav(expected_file_name)
+    assert np.array_equal(expected_audio.audio_segment,
+                          current_audio.audio_segment)
 
 
 def test_indexing():
-    assert False
+    current_audio.from_wav(basic_filename)
+
+    reversed_segment = np.array(current_audio.audio_segment)
+    for i in range(0, len(current_audio.audio_segment)):
+        reversed_segment[i] = current_audio.at(-i)
+    current_audio.audio_segment = reversed_segment
+
+    expected_file_name = "tests/audios/test_reversed.wav"
+    expected_audio.from_wav(expected_file_name)
+    assert np.array_equal(expected_audio.audio_segment,
+                          current_audio.audio_segment)
 
 
 def test_slicing():
-    assert False
+    current_audio.from_wav(basic_filename)
+
+    current_audio.audio_segment = current_audio.get_slice(
+        0,
+        len(current_audio.audio_segment) // 2)
+    expected_file_name = "tests/audios/test_split.wav"
+    expected_audio.from_wav(expected_file_name)
+    assert np.array_equal(expected_audio.audio_segment,
+                          current_audio.audio_segment)
 
 
 def test_deletion():
-    assert False
+    current_audio.from_wav(basic_filename)
+    current_audio.delete(0, len(current_audio.audio_segment) // 2)
+    expected_file_name = "tests/audios/test_deleted.wav"
+    expected_audio.from_wav(expected_file_name)
+    assert np.array_equal(expected_audio.audio_segment,
+                          current_audio.audio_segment)
 
 
-def test_insert_part():
-    assert False
+def test_change_the_volume():
+    current_audio.from_wav(basic_filename)
+    current_audio.change_volume(10)
+    expected_file_name = "tests/audios/test_changed_volume.wav"
+    expected_audio.from_wav(expected_file_name)
+    assert np.array_equal(expected_audio.audio_segment,
+                          current_audio.audio_segment)
 
 
-def test_change_the_volume_on_a_segment():
-    assert False
-
-
-def test_smooth_appearance():
-    assert False
-
-
-def test_smooth_fade_out():
-    assert False
+def test_change_speed():
+    current_audio.from_wav(basic_filename)
+    current_audio.change_speed(0.75)
+    expected_file_name = "tests/audios/test_changed_speed.wav"
+    expected_audio.from_wav(expected_file_name)
+    assert expected_audio.rate == current_audio.rate
